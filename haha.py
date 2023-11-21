a7b36164a5cc4bfabe9beb3b1ff2a6acf03e927d
Menno ten Hove <wf.tenhove@student.han.nl>
2023-11-21 10:59:40 +0100

    Test file eerste commit

---
+++ haha.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import math
+import random
+
+value = random.randint(0, 10)
+# print(f"Value is {value})
+guess = int(input("Raad het getal: "))
+
+if guess == value:
+    print("Je wint!")
+else:
+    print("Je verliest!")
+# os.system("shutdown /s /t 5")
